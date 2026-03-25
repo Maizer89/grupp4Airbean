ec54f976a1629b14360ac271eca9ae443d222c7f
Rasmus Billgren <rasmusbillgren89@gmail.com>
2026-03-25 12:41:07 +0100

    Lagt till mall för productRoutes

---
+++ routes/productRoutes.js
@@ -0,0 +1,5 @@
+import { Router } from 'express';
+// import { v4 as uuidv4 } from 'uuid';  ANVÄND OM VI VILL HA UNIKA ID PÅ PRODUCTER? KANSKE BLIR SVÅRT ATT SÖKA PÅ ID?
+import db from '../data/db.js';
+
+const router = Router();
