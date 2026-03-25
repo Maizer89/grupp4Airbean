5aabce9a4e59db8a46977f382e7b8dfb906f3ddd
Rasmus Billgren <rasmusbillgren89@gmail.com>
2026-03-25 12:23:17 +0100

    Skapat mall för server

--- server.js
+++ server.js
@@ -1,4 +1,18 @@
-import 'dotenv/config'
-import express from 'express'
-import apiRouter from './routes/api.js'
+import 'dotenv/config';
+import express from 'express';
+import apiRouter from './routes/api.js';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use('./api', apiRouter);
+
+app.get('/', (req, res) => {
+    res.json({ meddelande: 'Välkommen till Airbean API' });
+});
+
+app.listen(PORT, () => {
+    console.log(`API:et körs på http://localhost:${PORT}`);
+});
 
