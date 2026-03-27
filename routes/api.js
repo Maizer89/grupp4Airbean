244c47aa21dd7f158e3c86632f366f25ddaddc47
Rasmus Billgren <rasmusbillgren89@gmail.com>
2026-03-27 09:50:46 +0100

    fix/ ändrade import för userRoutes

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import { requireApiKey } from '../middlewear/requireApiKey.js'
-import { userRoutes } from './userRoutes.js'
+import userRoutes from './userRoutes.js'
 
 
 const router = Router();
