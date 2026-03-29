4d02626eadab7e9174bfafcf805a6bea60868b26
Rasmus Billgren <rasmusbillgren89@gmail.com>
2026-03-29 14:22:04 +0200

    ändrade name från api till app

---
+++ routes/app.js
@@ -0,0 +1,11 @@
+import { Router } from 'express'
+import { requireApiKey } from '../middlewear/requireApiKey.js'
+import userRoutes from './userRoutes.js'
+
+
+const router = Router();
+
+router.use(requireApiKey);
+router.use('/users', userRoutes)
+
+export default router;
