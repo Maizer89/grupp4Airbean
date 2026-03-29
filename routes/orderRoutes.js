1e6ea8105de286917e8ea3bf5daa6b64e4f5fbc6
Rasmus Billgren <rasmusbillgren89@gmail.com>
2026-03-29 14:01:53 +0200

    färdigställde order post

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,13 +1,35 @@
 import { Router } from 'express';
-// import { v4 as uuidv4 } from 'uuid';  ANVÄND OM VI VILL HA UNIKA ID PÅ PRODUKTER? KANSKE BLIR SVÅRT ATT SÖKA PÅ ID?
+import orderValidation from '../middlewear/orderValidation.js';
 import db from '../data/db.js';
 
 const router = Router();
 
-router.get('/', (req, res) => {
-    const userId = req.user.id;
 
-    const orders = db.prepare('SELECT id, total_amount, status, shipping_address FROM orders WHERE user_id = ?').all(userId)
+router.post('/', orderValidation, (req, res) => {
+    const {totalAmount, shippingAddress, orderItems } = req.body;
+    const deliveryTime = "10-15min leveranstid"
 
-    res.json(orders)
+    const order = db.prepare(`
+        INSERT INTO orders (total_amount, shipping_address, delivery_time)
+        VALUES (?, ?, ?)
+        `).run(totalAmount, shippingAddress, deliveryTime);
+
+    const orderId = order.lastInsertRowid;
+
+    const stmt = db.prepare(`
+        INSERT INTO order_items (order_id, product_id, price, quantity, product_name)
+        VALUE (?, ?, ?, ?, ?)
+        `)
+
+    for (let item of orderItems) {
+        stmt.run(orderId, item.productId, item.price, item.quantity, item.productName)
+    }
+
+    res.json({
+        id: orderId,
+        totalAmount,
+        shippingAddress,
+        deliveryTime,
+        orderItems
+    })
 })
