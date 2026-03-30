cc96c73a0d51e549c5b72eca35c2e330d449db4d
Rasmus Billgren <rasmusbillgren89@gmail.com>
2026-03-30 09:40:32 +0200

    la till order route

--- routes/app.js
+++ routes/app.js
@@ -1,11 +1,13 @@
 import { Router } from 'express'
 import { requireApiKey } from '../middlewear/requireApiKey.js'
 import userRoutes from './userRoutes.js'
+import orderRoutes from './orderRoutes.js'
 
 
 const router = Router();
 
 router.use(requireApiKey);
 router.use('/users', userRoutes)
+router.use('/orders', orderRoutes)
 
 export default router;
