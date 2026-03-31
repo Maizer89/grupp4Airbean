a01670f30d7213bd3bd2f986391a91107dadbdee
Rasmus Billgren <rasmusbillgren89@gmail.com>
2026-03-31 13:23:09 +0200

    la till hantering för user/gäst

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,45 +1,51 @@
 import { Router } from 'express';
 import orderValidation from '../middleware/orderValidation.js';
+import loggedInAuth from '../middleware/loggedInAuth.js';
+import validateProducts from '../middleware/validateProducts.js';
 import db from '../data/db.js';
 
 const router = Router();
 
-
-router.post('/', orderValidation, (req, res) => {
+router.post('/', loggedInAuth, orderValidation, validateProducts, (req, res) => {
     const {shippingAddress, orderItems } = req.body;
+    const userId = req.user ? req.user.id : null
     const deliveryTime = "10-15min leveranstid";
     let totalAmount = 0;
+    const products = []
 
     for (let item of orderItems) {
-        const product = db.prepare('SELECT price FROM products WHERE id = ?').get(item.productId);
+        const product = db.prepare('SELECT price, title, desc, createdAt, updatedAt FROM products WHERE id = ?').get(item.product_id);
 
-        const itemTotal = product.price * item.quantity;
-        totalAmount += itemTotal
+        products.push(product)
+        totalAmount += product.price * item.quantity
     }
 
     const createdAt = new Date().toISOString()
     const order = db.prepare(`
-        INSERT INTO orders (total_amount, shipping_address, delivery_time, createdAt)
-        VALUES (?, ?, ?, ?)
-        `).run(totalAmount, shippingAddress, deliveryTime, createdAt);
+        INSERT INTO orders (total_amount, shipping_address, delivery_time, user_id createdAt)
+        VALUES (?, ?, ?, ?, ?)
+        `).run(userId, totalAmount, shippingAddress, deliveryTime, createdAt);
 
     const orderId = order.lastInsertRowid;
 
     const stmt = db.prepare(`
         INSERT INTO order_items (order_id, product_id, price, quantity, product_name, createdAt)
-        VALUE (?, ?, ?, ?, ?, ?)
+        VALUES (?, ?, ?, ?, ?, ?)
         `)
 
-    for (let item of orderItems) {
-        stmt.run(orderId, item.productId, item.price, item.quantity, item.productName)
-    }
+    orderItems.forEach(item, index => {
+        const product = products[index]
+        stmt.run(orderId, item.product_id, product.price, item.quantity, product.title, createdAt)
+    });
+    
 
     res.json({
         id: orderId,
         totalAmount,
         shippingAddress,
         deliveryTime,
-        orderItems
+        orderItems,
+        createdAt
     })
 })
 
