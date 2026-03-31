79e15e206ff654068c3145f45991c09cbe5b1d05
Nehac210401 <chatrathneha2@gmail.com>
2026-03-31 09:10:24 +0200

    added productroutes.js.

--- routes/app.js
+++ routes/app.js
@@ -2,6 +2,7 @@ import { Router } from 'express'
 import { requireApiKey } from '../middleware/requireApiKey.js'
 import userRoutes from './userRoutes.js'
 import orderRoutes from './orderRoutes.js'
+import productRoutes from './productRoutes.js';
 
 
 const router = Router();
@@ -9,5 +10,6 @@ const router = Router();
 router.use(requireApiKey);
 router.use('/users', userRoutes)
 router.use('/orders', orderRoutes)
+router.use('/', productRoutes)
 
 export default router;
