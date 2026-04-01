584cbc0ec3c44cfd20ee7390bd297ee21ba3ffc3
Rasmus Billgren <rasmusbillgren89@gmail.com>
2026-04-01 17:04:17 +0200

    la till GET för user-orders

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -8,13 +8,13 @@ const router = Router();
 
 router.post('/', loggedInAuth, orderValidation, validateProducts, (req, res) => {
     const {shippingAddress, orderItems } = req.body;
-    const userId = req.user ? req.user.id : null
+    const userId = req.user?.id || null
     const deliveryTime = "10-15min leveranstid";
     let totalAmount = 0;
     const products = []
 
     for (let item of orderItems) {
-        const product = db.prepare('SELECT price, title, desc, createdAt, updatedAt FROM products WHERE id = ?').get(item.product_id);
+        const product = db.prepare('SELECT price, title FROM products WHERE id = ?').get(item.product_id);
 
         products.push(product)
         totalAmount += product.price * item.quantity
@@ -22,9 +22,9 @@ router.post('/', loggedInAuth, orderValidation, validateProducts, (req, res) => 
 
     const createdAt = new Date().toISOString()
     const order = db.prepare(`
-        INSERT INTO orders (total_amount, shipping_address, delivery_time, user_id createdAt)
+        INSERT INTO orders (total_amount, shipping_address, delivery_time, user_id, createdAt)
         VALUES (?, ?, ?, ?, ?)
-        `).run(userId, totalAmount, shippingAddress, deliveryTime, createdAt);
+        `).run(totalAmount, shippingAddress, deliveryTime, userId, createdAt);
 
     const orderId = order.lastInsertRowid;
 
@@ -33,7 +33,7 @@ router.post('/', loggedInAuth, orderValidation, validateProducts, (req, res) => 
         VALUES (?, ?, ?, ?, ?, ?)
         `)
 
-    orderItems.forEach(item, index => {
+    orderItems.forEach((item, index) => {
         const product = products[index]
         stmt.run(orderId, item.product_id, product.price, item.quantity, product.title, createdAt)
     });
@@ -41,12 +41,35 @@ router.post('/', loggedInAuth, orderValidation, validateProducts, (req, res) => 
 
     res.json({
         id: orderId,
+        userId,
         totalAmount,
         shippingAddress,
         deliveryTime,
         orderItems,
         createdAt
     })
+})
+
+router.get('/my-orders', loggedInAuth, (req, res) => {
+    const userId = req.user?.id;
+
+    if (!userId) {
+        return res.status(401).json({ fel: "Logga in för att se orders" });
+    }
+
+    const orders = db.prepare(`SELECT id, total_amount, shipping_address, delivery_time, COALESCE(updatedAt, createdAt) AS date FROM orders WHERE user_id = ?`).all(userId)
+    const orderItems = db.prepare(`SELECT product_id, price, quantity, product_name FROM order_items WHERE order_id = ?`)
+
+    const result = orders.map(order => {
+        const items = orderItems.all(order.id)
+
+        return {
+            ...order,
+            items
+        }
+    })
+
+    res.json(result)
 })
 
 export default router;
