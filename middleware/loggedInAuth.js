c41f1d9d59048b3223770e063c5c2cabceee08b9
Rasmus Billgren <rasmusbillgren89@gmail.com>
2026-04-01 17:02:47 +0200

    la till kontroll för userId

--- middleware/loggedInAuth.js
+++ middleware/loggedInAuth.js
@@ -1,14 +1,23 @@
+import db from '../data/db.js'
+
 export default function loggedInAuth(req, res, next) {
     const userId = req.headers['x-user-id'];
-
+    
     if(!userId) {
         req.user = null;
         return next()
     }
-       
+    
     if(typeof userId !== 'string') {
         return res.status(400).json({ fel: "Ogiltigt userId" })
     }
+
+    const user = db.prepare('SELECT id FROM users WHERE id = ?').get(userId)
+    
+    if(!user) {
+        return res.status(400).json({ fel: "Användaren finns inte" })
+    } 
+       
     req.user = { id: userId };
     next();
 }
