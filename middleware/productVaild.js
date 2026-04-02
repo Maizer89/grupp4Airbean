115f3e6e72443f0315e73d9eb68397b576ce2b6a
Nehac210401 <chatrathneha2@gmail.com>
2026-04-02 09:27:52 +0200

    Delete validation as it is already in the productRoutes.js

--- middleware/productVaild.js
+++
@@ -1,9 +0,0 @@
-export function validateProduct(req, res, next) {
-    const { title, price, desc } = req.body;
-    
-    if (!title || !price || !desc) {
-        return res.status(400).json({ fel: "Title, price och desc krävs"});
-    }
-    
-    next();
-}
